28a2c9e4d14286d9eadb142f4492c5c91fa87b2a
Pedro Hasse Niemczewski <pedro.hasse@dtidigital.com.br>
2025-09-03 14:51:42 -0300

    feat: extensions e registro de dependências centralizados, além de estruturação do program

---
+++ LearningLoop.GerenciamentoAlunosApp/LearningLoop.GerenciamentoAlunosApp/Extensions/RepositoryExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace LearningLoop.GerenciamentoAlunosApp.Extensions
+{
+    public static class RepositoryExtensions
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
--- LearningLoop.GerenciamentoAlunosApp/LearningLoop.GerenciamentoAlunosApp/Program.cs
+++ LearningLoop.GerenciamentoAlunosApp/LearningLoop.GerenciamentoAlunosApp/Program.cs
@@ -1,25 +1,51 @@
-var builder = WebApplication.CreateBuilder(args);
+using LearningLoop.GerenciamentoAlunosApp.DI;
+using LearningLoop.GerenciamentoAlunosApp.Extensions;
+using Microsoft.AspNetCore.Mvc;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace LearningLoop.GerenciamentoAlunosApp
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        protected Program()
+        {
+        }
+
+        public static void Main(string[] args)
+        {
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+            ConfigureServices(builder);
+
+            WebApplication app = builder.Build();
+
+            ConfigureApp(app);
+
+            app.Run();
+        }
+
+        private static void ConfigureServices(WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+
+            builder.Services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
+
+            builder.Services.AddDependencyInjection(builder.Configuration);
+        }
+
+        private static void ConfigureApp(WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseInfra();
+            app.UseApiLayer();
+            app.MapApiEndpoints();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
