db62601c6488a1bda3b0fc3bcef6d7968a99704a
Pedro Hasse Niemczewski <pedro.hasse@dtidigital.com.br>
2025-09-03 15:27:23 -0300

    feat: implementa DTOs

---
+++ LearningLoop.GerenciamentoAlunosApp/LearningLoop.GerenciamentoAlunosApp/DTO/AlunoDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace LearningLoop.GerenciamentoAlunosApp.DTO
+{
+    public class AlunoDTO
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Curso { get; set; }
+        public bool Status { get; set; }
+        public DateOnly DataCriacao { get; set; }
+        public DateOnly DataAtualizacao { get; set; }
+    }
+}
