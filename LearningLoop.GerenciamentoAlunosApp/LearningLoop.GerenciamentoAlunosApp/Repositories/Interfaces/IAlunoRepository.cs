fc3ae333d375e845a9da391df1faab17f0e35183
Pedro Hasse Niemczewski <pedro.hasse@dtidigital.com.br>
2025-09-04 09:56:40 -0300

    feat: Implementar os repositórios básicos para usuários e alunos

---
+++ LearningLoop.GerenciamentoAlunosApp/LearningLoop.GerenciamentoAlunosApp/Repositories/Interfaces/IAlunoRepository.cs
@@ -0,0 +1,12 @@
+﻿using LearningLoop.GerenciamentoAlunosApp.Arguments;
+using LearningLoop.GerenciamentoAlunosApp.Models;
+
+namespace LearningLoop.GerenciamentoAlunosApp.Repositories.Interfaces
+{
+    public interface IAlunoRepository
+    {
+        Task<AlunoModel> CriarAlunoAsync(AlunoArgument argument);
+        Task<IEnumerable<AlunoModel>> ObterTodosAlunosAsync();
+        Task<AlunoModel?> ObterAlunoPorIdAsync(int id);
+    }
+}
---
+++ LearningLoop.GerenciamentoAlunosApp/LearningLoop.GerenciamentoAlunosApp/Repositories/UsuarioRepository.cs
@@ -0,0 +1,15 @@
+﻿using LearningLoop.GerenciamentoAlunosApp.Arguments;
+using LearningLoop.GerenciamentoAlunosApp.Models;
+using LearningLoop.GerenciamentoAlunosApp.Repositories.Base;
+using LearningLoop.GerenciamentoAlunosApp.Repositories.Interfaces;
+
+namespace LearningLoop.GerenciamentoAlunosApp.Repositories
+{
+    public class UsuarioRepository : PostgresBaseRepository, IUsuarioRepository
+    {
+        public UsuarioRepository(ILogger<UsuarioRepository> logger, IConfiguration configuration)
+            : base(logger, configuration)
+        {
+        }
+    }
+}
